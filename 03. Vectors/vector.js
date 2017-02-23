84cec77b9438fc2c0007af21a438ed464c2b2221
Michael Duong <Michael Duong>
2017-02-22 18:57:40 -0800

    linked list insert

--- 03. Vectors/vector.js
+++ 03. Vectors/vector.js
@@ -15,31 +15,94 @@
 // [ ] .contains() function to return true/false whether a given value exists in the Vector
 //
 var Vector = function(initialCapacity, maxCapacity) {
-  this.storage = ...;
-  this.capacity = initialcapacity || 16;  // Default to array size 16
+  this.storage = [];
+  this.capacity = initialCapacity || 16;  // Default to array size 16
   this.max = maxCapacity || 1 << 24;      // Default to max Vector size 16,777,216
   this.length = 0;
 };
 
+Vector.prototype.checkCapacityUpper = function(){
+  if(this.length + 1 > this.max){
+    console.log('error! exceeded maximum memory usage');
+    return false;
+  }else if (this.length + 1 > this.capacity){
+    this.capacity *= 2;
+  }
+  return true;
+};
+
+Vector.prototype.checkCapacityLower = function(){
+  if(this.capacity > 16 && this.length - 1 <= this.capacity/4){
+    this.capacity = this.capacity/2;
+  }
+};
+
+Vector.prototype.checkBounds = function(index){
+  if (index < 0 || index > this.length){
+    console.log('error. index out of bounds');
+    return false;
+  }
+  return true;
+};
 
 Vector.prototype.insert = function(index, value) {
-  // ...
+  if (!this.checkBounds(index) || !this.checkCapacityUpper()){
+    return;
+  }
+  var newArray = [];
+  for (var i = 0; i < index; i++){
+    newArray[i] = this.storage[i];
+  }
+  newArray[i] = value;
+  for (var i = index + 1; i <= this.storage.length; i++){
+    newArray[i] = this.storage[i-1];
+  }
+  this.storage=newArray;
+  this.length++;
+  return newArray;
 };
 
 Vector.prototype.add = function(value) {
-  // ...
+  if (!this.checkCapacityUpper()){
+      return;
+  }
+  if (this.storage.length){
+    this.storage[this.storage.length] = value;
+  }
+  else{
+    this.storage[0] = value;
+  }
+  this.length++;
+  return this.storage;
 };
 
 Vector.prototype.remove = function(index) {
-  // ...
+  if (!this.checkBounds(index)){
+      return;
+  }
+  this.checkCapacityLower();
+  for (var i = index; i < this.storage.length; i++ ){
+    this.storage[i] = this.storage[i+1];
+  }
+  this.storage.pop();
+  this.length--;
+  return this.storage;
 };
 
 Vector.prototype.get = function(index) {
-  // ...
+  if (!this.checkBounds(index)){
+      return;
+  }
+  return this.storage[index];
 };
 
 Vector.prototype.set = function(index, value) {
-  // ...
+  if (!this.checkBounds(index)){
+      return;
+  }
+  if (index < this.storage.length && index > 0){
+    this.storage[index] = value;
+  }
 };
 
 
--- 04. Lists/linkedList.js
+++ 04. Lists/linkedList.js
@@ -17,21 +17,54 @@ var LinkedListNode = function(data, next) {
 
 var LinkedList = function() {
   this.head = new LinkedListNode(null, null);
-  this.tail = head;
+  this.tail = this.head;
+  this.size = 0;
 };
 
 
 
 LinkedList.prototype.insert = function(index, value) {
-  // ...
+  if (index > this.size){
+    console.log('error, index not in list');
+    return;
+  }
+  if (index === null || index === undefined){
+    index = this.size+1;
+  }
+  var currentNode = this.head;
+  if (index === 0){
+    this.head = new LinkedListNode(value, this.head);
+    if (++this.size === 1){
+      this.tail = this.head;
+    }
+    return this.head;
+  }else{
+      for (var i = 0; i < index - 1; i++){
+          currentNode = currentNode.next;
+      }
+      currentNode.next = new LinkedListNode(value, currentNode.next);
+      if (index === ++this.size){
+          this.tail=currentNode.next;
+          this.tail.next = null;
+      }
+  }
+  return currentNode.next;
 };
 
 LinkedList.prototype.remove = function(index) {
-  // ...
+  var currentNode = this.head;
+  for (var i = 0; i < index-1; i++){
+
+  }
+
 };
 
 LinkedList.prototype.get = function (index) {
-  // ...
+  var node = this.head;
+  while(index > 0){
+    node = node.next;
+  }
+  return node.data;
 };
 
 LinkedList.prototype.set = function(index, value) {
@@ -39,10 +72,27 @@ LinkedList.prototype.set = function(index, value) {
 };
 
 LinkedList.prototype.find = function(value) {
-  // ...
+  var node = this.head;
+  var index = this.length;
+  while (node !== null && index > 0){
+    if (node.data === value){
+      return node;
+    }
+    node = node.next;
+    index--;
+  }
 };
 
 LinkedList.prototype.contains = function(value) {
   // ...
 };
 
+var myList = new LinkedList();
+myList.insert(0,0);
+myList.insert(0,1);
+myList.insert(0,2);
+myList.insert(0,3);
+myList.insert(2,22);
+myList.insert(2,33);
+myList.insert(6,444);
+console.log(myList);
