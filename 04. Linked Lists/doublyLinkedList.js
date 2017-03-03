fc090995088cadbb0207a8faea0f499a5ae8a71c
Michael Duong <Michael Duong>
2017-03-03 10:22:43 -0800

    added unit tests

--- 04. Linked Lists/doublyLinkedList.js
+++ 04. Linked Lists/doublyLinkedList.js
@@ -217,64 +217,72 @@ function toArray(fromWhichNode) {
   return result;
 }
 
+
 var list = new DoublyLinkedList();
-var x = null;
 
-console.log("Inserts");
-list.insert(null, 0);
-console.log("  List should be [0]: " + (toArray(list.head).equals([0])));
-list.insert(null, 1);
-list.insert(null, 2);
-list.insert(null, 3);
-console.log("  After inserting 3 more at tail, list should be [0, 1, 2, 3]: " + (toArray(list.head).equals([0, 1, 2, 3])));
+console.log("Inserts First & Before");
+var n0 = list.insert(0, 0);
+console.log("  insert(0, 0) should yield [0]: " + (toArray(list.head).equals([0])));
+console.log("  head should be n0: " + (n0 === list.head));
+console.log("  tail should be n0: " + (n0 === list.tail));
+var n00 = list.insert(0, -1);
+console.log("  insert(0, -1) should yield [-1, 0]: " + (toArray(list.head).equals([-1, 0])));
+console.log("  head should be -1: " + (list.head.data === -1));
+console.log("  tail should be 0: " + (list.tail.data === 0));
+var n1 = list.insert(1, 1);
+console.log("  insert(1, 1) should yield [-1, 1, 0]: " + (toArray(list.head).equals([-1, 1, 0])));
+var n2 = list.insert(2, 2);
+console.log("  insert(2, 2) should yield [-1, 1, 2, 0]: " + toArray(list.head).equals([-1, 1, 2, 0]));
+console.log("  tail should be 0: " + (list.tail.data === 0));
+var n4 = list.insert(null, 4);
+console.log("  insert(null, 4) should yield [-1, 1, 2, 4, 0]:" + (toArray(list.head).equals([-1, 1, 2, 4, 0])));
+console.log("  tail should be 4: " + (list.tail.data === 0));
 
-list.insert(0, 10);
-console.log("  After inserting at head, list should be [10, 0, 1, 2, 3]: " + (toArray(list.head).equals([10, 0, 1, 2, 3])));
+console.log("Inserts After");
+var n5 = list.insertAfter(n2, 10);
+console.log("  insertAfter([2], 10) should yield [-1, 1, 2, 10, 4, 0]: " + (toArray(list.head).equals([-1, 1, 2, 10, 4, 0])));
+var n6 = list.insertAfter(list.head, 6);
+console.log("  insertAfter([head], 6) should yield [-1, 6, 1, 2, 10, 4, 0]: " + (toArray(list.head).equals([-1, 6, 1, 2, 10, 4, 0])));
+var n7 = list.insertAfter(list.tail, 7);
+console.log("  insertAfter([tail], 7) should yield [-1, 6, 1, 2, 10, 4, 0, 7]:" + (toArray(list.head).equals([-1, 6, 1, 2, 10, 4, 0, 7])));
 
-list.insert(2, 20);
-console.log("  insert(2, 20) should be [10, 0, 20, 1, 2, 3]: " + (toArray(list.head).equals([10, 0, 20, 1, 2, 3])));
+console.log("Inserts Before");
+var n7 = list.insertBefore(list.head, 11);
+console.log("  insertBefore([head], 11) should yield [11, -1, 6, 1, 2, 10, 4, 0, 7]: " + (toArray(list.head).equals([11, -1, 6, 1, 2, 10, 4, 0, 7])));
+var n8 = list.insertBefore(list.tail, 12);
+console.log("  insertBefore([tail], 12) should yield [11, -1, 6, 1, 2, 10, 4, 0, 12, 7]: " + (toArray(list.head).equals([11, -1, 6, 1, 2, 10, 4, 0, 12, 7])));
+var n9 = list.insertBefore(n5, 55);
+console.log("  insertBefore([5], 55) should yield [11, -1, 6, 1, 2, 55, 10, 4, 0, 12, 7]: " + (toArray(list.head).equals([11, -1, 6, 1, 2, 55, 10, 4, 0, 12, 7])));
+console.log("  head should be 11: " + (list.head.data === 11));
+console.log("  tail should be 7: " + (list.tail.data === 7));
 
-list.insert(5, 50);
-console.log("  insert(5, 50) [the tail] should be [10, 0, 20, 1, 2, 50, 3]: " + (toArray(list.head).equals([10, 0, 20, 1, 2, 50, 3])));
+console.log("Finds & Contains");
+console.log("  list.find(12) returns 12: " + (list.find(12).data === 12));
+console.log("  list.find(0) returns 11: " + (list.find(11).data === 11));
+console.log("  list.find(7) returns 7:" + (list.find(7).data === 7));
+console.log("  list.find(100) returns null: " + (list.find(100) === null));
+console.log("  list.contains(11) is true: " + (list.contains(11) === true));
+console.log("  list.contains(55) is true: " + (list.contains(55) === true));
+console.log("  list.contains(7) is true: " + (list.contains(7) === true));
+console.log("  list.contains(221) is false: " + (list.contains(221) === false));
 
-console.log("Search");
-x = list.find(50);
-console.log("  find(50) should return { data: 50, next: { data: 3, ... }}: " + (x.data === 50 && x.next.data === 3));
-x = list.find(3);
-console.log("  find(3) should return { data: 3, ... }: " + (x.data === 3 && x.next === null));
-x = list.find(10);
-console.log("  find(10) should return { data: 10, ...}: " + (x.data === 10));
-x = list.find(100);
-console.log("  find(100) should return null: " + (x === null));
-console.log("  contains(10) should be true: " + (list.contains(10) !==null));
-console.log("  contains(50) should be true: " + (list.contains(50) !== null));
-console.log("  contains(3) should be true: " + (list.contains(3) !== null));
-console.log("  contains(100) should be false: " + (list.contains(100) === false));
+console.log("Removes");
+var r1 = list.remove(0);
+console.log("  remove(0) should yield [-1, 6, 1, 2, 55, 10, 4, 0, 12, 7]: " + (toArray(list.head).equals([-1, 6, 1, 2, 55, 10, 4, 0, 12, 7])));
+var r2 = list.remove(9);
+console.log("  remove(9) should yield [-1, 6, 1, 2, 55, 10, 4, 0, 12]: " + (toArray(list.head).equals([-1, 6, 1, 2, 55, 10, 4, 0, 12])));
+var r3 = list.remove(4);
+console.log("  remove(4) should yield [-1, 6, 1, 2, 10, 4, 0, 12]: " + (toArray(list.head).equals([-1, 6, 1, 2, 10, 4, 0, 12])));
 
-console.log("Get");
-console.log("  get(0) should return 10: " + (list.get(0) === 10));
-console.log("  get(3) should return 1: " + (list.get(3) === 1));
-console.log("  get(6) should return 3: " + (list.get(6) === 3));
-console.log("  get(10) should return null: " + (list.get(10) === null));
+console.log("Gets & Sets");
+for (var i=0; i<8; i++) {
+  list.set(i, i);
+}
+console.log("  set(0 ... 7) should yield [0, 1, 2, 3, 4, 5, 6, 7]: " + (toArray(list.head).equals([0, 1, 2, 3, 4, 5, 6, 7])));
+var a = [];
+for (var i=7; i>=0; i--) {
+  a.push(list.get(i));
+}
+console.log("  get(7 ... 0) should yield [7, 6, 5, 4, 3, 2, 1, 0]: " + (a.equals([7, 6, 5, 4, 3, 2, 1, 0])));
 
-console.log("Set");
-list.set(0, 100);
-list.set(3, 103);
-list.set(6, 106);
-console.log("  get(0) should return 100: " + (list.get(0) === 100));
-console.log("  get(3) should return 103: " + (list.get(3) === 103));
-console.log("  get(6) should return 106: " + (list.get(6) === 106));
-
-console.log("Remove");
-list.remove(6);
-console.log("  remove(6) should yield [100, 0, 20, 103, 2, 5]: " + (toArray(list.head).equals([100, 0, 20, 103, 2, 50])));
-list.remove(3);
-console.log("  remove(3) should yield [100, 0, 20, 2, 50]: " + (toArray(list.head).equals([100, 0, 20, 2, 50])));
-list.remove(0);
-console.log("  remove(0) should yield [0, 20, 2, 50]: " + (toArray(list.head).equals([0, 20, 2, 50])));
-list.remove(1);
-console.log("  remove(1) should yield [0, 2, 50]: " + (toArray(list.head).equals([0, 2, 50])));
-
-console.log("Housekeeping");
-console.log("  head should be { 0 }: " + (list.head.data === 0));
-console.log("  tail should be { 50 }: " + (list.tail.data === 50));
+console.log(toArray(list.head));
