0204141be3e35e2029c030d285aa3ae6a465538c
Michael Duong <Michael Duong>
2017-03-15 23:02:53 -0700

    implemented binary search tree add

--- 12. Trees/binarySearchTree.js
+++ 12. Trees/binarySearchTree.js
@@ -6,3 +6,50 @@
 //  [ ] find node functionality
 //  [ ] remove node functionality (extra credit)
 //
+
+var Node = function(value, left, right){
+  this.data = value;
+  this.left = left;
+  this.right= right;
+}
+
+var BinarySearchTree = function(){
+  this.root = new Node();
+}
+
+BinarySearchTree.prototype.find = function (value, node){
+  node = node || this.root;
+  if (node.data === value){
+    return node;
+  }
+  var found = this.find(value, node.left);
+  if (!found){
+    found = this.find(value, node.right);
+  }
+  return found;
+}
+
+BinarySearchTree.prototype.add = function(value, node, direction){
+  if (!this.root.data){
+    this.root.data = value;
+    return true;
+  }
+
+  node = node || this.root;
+
+  if (!node[direction] && node[direction] !== 0){
+    node[direction] = new Node(value);
+    return true;
+  }
+
+  if (value < node.data){
+    direction = left;
+  }
+  else if (value > node.data){
+    direction = right;
+  }
+  else {
+    return false;
+  }
+  this.add(value, node, direction);
+}
--- 12. Trees/binaryTree.js
+++ 12. Trees/binaryTree.js
@@ -6,3 +6,37 @@
 //  [ ] find node functionality
 //  [ ] remove node functionality (extra credit)
 //
+
+
+var Node = function(value, left, right){
+  this.data = value;
+  this.left = left;
+  this.right= right;
+}
+
+var BinaryTree = function(){
+  this.root = new Node();
+}
+
+BinaryTree.prototype.find = function (value, node){
+  node = node || this.root;
+  if (node.data === value){
+    return node;
+  }
+  var found = find(value, node.left);
+  if (!found){
+    found = find(value, node.right);
+  }
+  return found;
+}
+
+BinaryTree.prototype.add = function(value, parent){
+  if (!parent){
+    var node = new Node(value, this.root);
+    this.root = node;
+  }else{
+  var node = new Node(value, parent.left);
+  parent.left = node;
+  }
+return true;
+}
